e50fa14240d50f8d485eb9e6be941b3d08572cfe
Jozxy <167943267+Jozxy@users.noreply.github.com>
2026-05-20 03:15:27 -0600

    Change fallback API URL to HTTPS
    Updated the fallback API URL to use HTTPS.

--- src/config/api.js
+++ src/config/api.js
@@ -26,7 +26,7 @@ export async function getApiUrl() {
     console.error('❌ Error cargando config.json:', error);
     
     // ✅ Fallback opcional, pero mejor lanzar el error en producción
-    const fallback = 'ftp-fulage.alwaysdata.net/api';
+    const fallback = 'https://fulage.alwaysdata.net/api';
     console.warn(`⚠️ Usando fallback: ${fallback}`);
     return fallback;
   }
